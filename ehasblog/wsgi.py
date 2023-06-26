4641c9d0b72798bddff86b364e2590b9a60b92e1
Michael Ibrahim <segunmichael24@gmail.com>
2023-06-26 16:35:54 +0100

    Correction in the wsgi.py file

--- ehasblog/wsgi.py
+++ ehasblog/wsgi.py
@@ -7,32 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ehasblog.settings')
-
-# application = get_wsgi_application()
-
-"""Django's command-line utility for administrative tasks."""
 import os
-import sys
 
+from django.core.wsgi import get_wsgi_application
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ehasblog.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ehasblog.settings")
 
-
-if __name__ == "__main__":
-    main()
+application = get_wsgi_application()
