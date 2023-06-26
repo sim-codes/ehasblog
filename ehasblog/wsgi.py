5b1602af278ba90e82cd91f5719edc0258f2092e
Michael Ibrahim <segunmichael24@gmail.com>
2023-06-26 16:08:32 +0100

    Update FILES for vercel

--- ehasblog/wsgi.py
+++ ehasblog/wsgi.py
@@ -15,8 +15,24 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 # application = get_wsgi_application()
 
+"""Django's command-line utility for administrative tasks."""
 import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
-application = get_wsgi_application()
-app = application
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ehasblog.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
